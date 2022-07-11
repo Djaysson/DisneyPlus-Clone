1ad738bddb8206b87b86a7e75a0c387ccfb452e9
Djaysson <djayson.dev@gmail.com>
2022-07-11 12:48:26 -0300

    FEAT(NavigationRoutes):incorporate react-router-dom and add initial navigation route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import { Header } from "./components/Header";
-import { Home } from "./pages/Home";
+import { NavigationRoutes } from "./router";
 import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <GlobalStyle />
+      <Router>
+        <Header />
+        <NavigationRoutes />
+        <GlobalStyle />
+      </Router>
     </>
   );
 }
